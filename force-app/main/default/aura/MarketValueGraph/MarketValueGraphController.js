3c99a85ee9054b4009c6c315edd46f8ac5af16c6
Andrew Wensley <awensley@u.rochester.edu>
2021-09-22 17:33:58 -0400

    Added chart for market price history Connected api data to chart Modified API handler for flexibility Set business products to autopopulate Psuhing remote site settings

--- force-app/main/default/aura/MarketValueGraph/MarketValueGraphController.js
+++ force-app/main/default/aura/MarketValueGraph/MarketValueGraphController.js
@@ -2,30 +2,74 @@
     myAction : function(component, event, helper) {
         
     },
-    chartSetup : function(component, event, helper) {
-    	component.set("v.content", "changed content");
+    
+    setup : function(component, event, helper) {
         
         let sampleData = component.get("c.sampleData");
+        sampleData.setParams({rowsInput: 5})
         sampleData.setCallback(this, function(response) {
             var metalsMap = response.getReturnValue();
             var metalsList = helper.populateMetalsList(metalsMap);
             component.set("v.metals", metalsList);
-                               })
+            
+            
+            var element = component.find("chart").getElement();
+            var ctx = element.getContext("2d");
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: helper.populateLabels(5),
+                    datasets: helper.populateDatasets(metalsMap)
+                }, 
+                options: {
+                    responsive: false
+                }
+            });
+        })
         
         $A.enqueueAction(sampleData);
-	},
+      },
     
-        actualData : function(component, event, helper) {
-    	component.set("v.content", "loading");
+    fakeData : function(component, event, helper) {
+        component.set("v.content", "changed content");
+        
+        let sampleData = component.get("c.sampleData");
+        sampleData.setParams({rowsInput: 5})
+        sampleData.setCallback(this, function(response) {
+            var metalsMap = response.getReturnValue();
+            var metalsList = helper.populateMetalsList(metalsMap);
+            component.set("v.metals", metalsList);
+        })
+        
+        $A.enqueueAction(sampleData);
+        
+    },
+    
+    actualData : function(component, event, helper) { component.set("v.content", "loading");
         
         let realData = component.get("c.realData");
+        realData.setParams({rowsInput: 15});
         realData.setCallback(this, function(response) {
             var metalsMap = response.getReturnValue();
             var metalsList = helper.populateMetalsList(metalsMap);
             component.set("v.metals", metalsList);
             component.set("v.content", "complete");
-                               })
+            
+            
+            var element = component.find("chart").getElement();
+            var ctx = element.getContext("2d");
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: helper.populateLabels(15),
+                    datasets: helper.populateDatasets(metalsMap)
+                }, 
+                options: {
+                    responsive: false
+                }
+            });
+        })
         
         $A.enqueueAction(realData);
-	}
- })
+    }
+})
--- force-app/main/default/aura/MarketValueGraph/MarketValueGraphHelper.js
+++ force-app/main/default/aura/MarketValueGraph/MarketValueGraphHelper.js
@@ -2,6 +2,70 @@
     helperMethod : function() {
         
     },
+    
+    populateDatasets : function(metalsMap) {
+        var dataSets = [];
+        var colors = ['rgb(200, 200, 00)',
+                     'rgb(200, 200, 200)',
+                     'rgb(50, 200, 50)',
+                     'rgb(200, 100, 00)',
+                     'rgb(200, 00, 200)',
+                     'rgb(200, 00, 00)',
+                     'rgb(00, 00, 00)',
+                     'rgb(00, 200, 200)',
+                     'rgb(50, 50, 200)',
+                     'rgb(150, 00, 50)'
+                     ];
+        var currentColor = 0;
+        
+        for (var key in metalsMap) {
+            var innerMap = metalsMap[key];
+            var data = [];
+            
+            for (var innerKey in innerMap) {
+                data.splice(0, 0, innerMap[innerKey]);
+            }
+            
+        var label = key;
+        
+        dataSets.push({
+                    label: label,
+                    data: data,
+                    fill: false,
+                    tension: 0,
+            		backgroundColor: colors[currentColor],
+            		borderColor: colors[currentColor]
+                });
+        currentColor += 1;
+        }
+        return dataSets;
+    },
+    
+    populateLabels : function(numberMonths) {
+        var labels = [];
+        var months = ['Jun',
+                      'May',
+                      'Apr',
+                      'Mar',
+                      'Feb',
+                      'Jan',
+                      'Dec',
+                      'Nov',
+                      'Oct',
+                      'Sep',
+                      'Aug',
+                      'Jul'
+                     ]
+        for (var i = 0; i < numberMonths; i++) {
+            var currentMonth = i;
+            while (currentMonth > 11) {
+                currentMonth = currentMonth - 12;
+            }
+        	labels.splice(0, 0, months[currentMonth]);
+        }
+        return labels;
+    },
+    
     populateMetalsList : function(metalsMap) {
         var metalsList = [];
         for (var key in metalsMap) {
