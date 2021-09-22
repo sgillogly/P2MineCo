5e7d6ba2237fd77d448b3794779b93b6d2d467b7
sgillogly <sgillogly718@gmail.com>
2021-09-22 08:55:25 -0600

    Home Page Updates

---
+++ force-app/main/default/aura/PopularSitesComponent/PopularSitesComponentController.js
@@ -0,0 +1,13 @@
+({
+	getImg : function(component, event, helper) {
+		helper.ApexUrlMethod(component, 0);
+	},
+    
+    getImg2 : function(component, event, helper) {
+		helper.ApexUrlMethod(component, 1);
+	},
+ 
+ 	getImg3 : function(component, event, helper) {
+		helper.ApexUrlMethod(component, 2);
+	}
+})
