91f882dd66bc7a11c1de1c4bd70b61575def224a
sgillogly <sgillogly718@gmail.com>
2021-09-27 09:59:45 -0600

    homePage more changes

--- force-app/main/default/aura/PopularSitesComponent/PopularSitesComponentController.js
+++ force-app/main/default/aura/PopularSitesComponent/PopularSitesComponentController.js
@@ -1,13 +1,13 @@
 ({
-	getImg : function(component, event, helper) {
-		helper.ApexUrlMethod(component, 0);
-	},
-    
-    getImg2 : function(component, event, helper) {
-		helper.ApexUrlMethod(component, 1);
-	},
- 
- 	getImg3 : function(component, event, helper) {
-		helper.ApexUrlMethod(component, 2);
-	}
+	loadImgs : function(component, event, helper) {
+        helper.ApexUrlMethod(component, 0);
+        helper.ApexSiteNameMethod(component, 0);
+        helper.ApexSiteOreMethod(component, 0);
+        helper.ApexUrlMethod(component, 1);
+        helper.ApexSiteNameMethod(component, 1);
+        helper.ApexSiteOreMethod(component, 1);
+        helper.ApexUrlMethod(component, 2);
+        helper.ApexSiteNameMethod(component, 2);
+        helper.ApexSiteOreMethod(component, 2);
+    }
 })
